dad2e5eb70881b919b7437ea1ceb94d23b85e6fd
Ross Reedstrom <reedstrm@gmail.com>
2018-02-01 14:27:58 -0600

    add a new-user (not yet publish approved) for tests

--- scripts/add_users.sh
+++ scripts/add_users.sh
@@ -2,6 +2,7 @@
 echo "user user1 user1 firstname1 lastname1 useremail1@localhost.net 
 user user2 user2 firstname2 lastname2 useremail2@localhost.net
 user user3 user3 firstname3 lastname3 useremail3@localhost.net
+new new1 new1 firstname1 lastname1 newemail1@localhost.net
 org siyavula siyavula siya-firstname siya-lastname siya-email@localhost.net
 org org1 org1 'org1 name' org1-name orgemail1@localhost.net
 org org2 org2 'org2 name' org2-name orgemail2@localhost.net
